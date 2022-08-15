3c738c17b18d7487c15b827bdce8338ded40826d
mustafaguler3 <mustafa.glr@hotmail.com>
2022-08-15 17:26:47 +0300

    add css transition on food hover

--- src/Menu/FoodGrid.js
+++ src/Menu/FoodGrid.js
@@ -22,9 +22,15 @@ export const Food = styled(Title)`
     background-size: cover;
     filter: contrast(75%);
     border-radius: 7px;
-    box-shadow:0px 0px 2px 0px grey;
+    margin-top:5px;
+    transition-property: box-shadow;
+    transition-duration: 1s;
+    box-shadow: 0px 0px 2px 0px grey;
     &:hover {
         cursor:pointer;
-        opacity:0.7;
+        box-shadow: 0px 0px 10px 0px grey;
+        filter: contrast(100%);
+        margin-top:0px;
+        margin-bottom:5px
     }
 `
